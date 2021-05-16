012a05c7df2c26de358abed38a5293c9c9865aca
Bjurn Overbeek <bbjoverbeek@gmail.com>
2021-05-16 15:19:23 +0200

    part B code done

---
+++ assignment_2/ANNOT4ML-assignment0.py
@@ -0,0 +1,56 @@
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+
+from sklearn.metrics import classification_report
+
+def read_corpus(corpus_file, use_sentiment):
+    documents = []
+    labels = []
+    with open(corpus_file, encoding='utf-8') as f:
+        for line in f:
+            tokens = line.strip().split()
+
+            documents.append(tokens[2:])
+
+            if use_sentiment:
+                # 2-class problem: positive vs negative
+                labels.append( tokens[1] )
+            else:
+                # 6-class problem: books, camera, dvd, health, music, software
+                labels.append( tokens[0] )
+
+    return documents, labels
+    
+# a dummy function that just returns its input
+def identity(x):
+    return x
+
+X, Y = read_corpus('trainset.txt', use_sentiment=False)
+split_point = int(0.75*len(X))
+Xtrain = X[:split_point]
+Ytrain = Y[:split_point]
+Xtest = X[split_point:]
+Ytest = Y[split_point:]
+
+# let's use the TF-IDF vectorizer
+tfidf = True
+
+# we use a dummy function as tokenizer and preprocessor,
+# since the texts are already preprocessed and tokenized.
+if tfidf:
+    vec = TfidfVectorizer(preprocessor = identity,
+                          tokenizer = identity)
+else:
+    vec = CountVectorizer(preprocessor = identity,
+                          tokenizer = identity)
+
+classifier = Pipeline( [('vec', vec),
+                        ('cls', MultinomialNB())] )
+
+
+classifier.fit(Xtrain, Ytrain)
+
+Yguess = classifier.predict(Xtest)
+
+print(classification_report(Ytest, Yguess, digits=3))
