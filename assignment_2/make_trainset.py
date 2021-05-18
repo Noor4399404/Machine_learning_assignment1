22b9ffec703c1a117da836c7577df73ddba67307
Bjurn Overbeek <bbjoverbeek@gmail.com>
2021-05-18 17:00:27 +0200

    final touches done

--- assignment_2/make_trainset.py
+++ assignment_2/make_trainset.py
@@ -2,11 +2,13 @@ import os
 
 
 def find_main_emotion(text):
+    '''This function finds and returns the
+    main emotion in the text from a .ann file'''
 
     for i in range(len(text)):
         if 'Main' in text[i]:
             return text[i-1].split()[1]
-    
+
     return 'None'
 
 
@@ -21,31 +23,37 @@ def main():
     for group_number in sorted(os.listdir(folderpath)):
         for i in range(1, 51):
             file_info = []
-            
+
             ann_file = (
-                folderpath + '/' + group_number + '/' + '{:0>2}'.format(i) + '.ann')
+                folderpath + '/' + group_number +
+                '/' + '{:0>2}'.format(i) + '.ann'
+                )
             with open(ann_file, 'r') as inp:
                 text = inp.readlines()
-            
+
             file_info.append(find_main_emotion(text))
-            file_info.append(group_number + '/' + '{:0>2}'.format(i) + '.txt')
+            file_info.append(
+                group_number + '/' + '{:0>2}'.format(i) + '.txt'
+                )
 
             text_file = (
-                folderpath + '/' + group_number + '/' + '{:0>2}'.format(i) + '.txt')
-            
+                folderpath + '/' + group_number +
+                '/' + '{:0>2}'.format(i) + '.txt'
+                )
+
             with open(text_file, 'r') as inp:
                 file_info.append(inp.read())
 
             trainset_info.append(file_info)
-
 
     for file_info in trainset_info:
         file_info = ' '.join(file_info)
         file_info = file_info.replace('\n', ' ')
         output_list.append(file_info)
 
-    with open('trainset.txt', 'w') as outp:
+    with open(output_file, 'w') as outp:
         outp.write('\n'.join(output_list))
+
 
 if __name__ == "__main__":
     main()
